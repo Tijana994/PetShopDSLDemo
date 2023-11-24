cf45a5259a36a07058884314cc2d60323b25464f
Tijana994 <tijana.vdn@gmail.com>
2023-11-24 17:41:05 +0100

    Example update

--- PetShopExample/src/petShopDemo/LokiCompany.java
+++ PetShopExample/src/petShopDemo/LokiCompany.java
@@ -5,7 +5,7 @@ public class LokiCompany {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		var shop = new LokiPetShop();
-		shop.createOffer("Loki");
+		shop.createOffer("granulica");
 	}
 
 }
--- PetShopExample/src/petShopDemo/LokiPetShop.java
+++ PetShopExample/src/petShopDemo/LokiPetShop.java
@@ -6,9 +6,20 @@ public class LokiPetShop {
 	public String OfferName;
 	
 	@OfferCreatedAnnotation(name = "OfferName")
-	public void createOffer(String name)
+	public Offer createOfferWithNotification(String name)
 	{
+		var offer = createOffer(name);
 		OfferName = name;
+		return offer;
+	}
+	
+	public Offer createOffer(String name)
+	{
+		var instance = PetShopDemoPackage.eINSTANCE;
+        PetShopDemoFactory factory = PetShopDemoFactory.eINSTANCE;
+		var offer = factory.createOffer();
+		OfferName = name;
+		return offer;
 	}
 
 }
